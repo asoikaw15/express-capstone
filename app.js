8e50df3d6a0f73cc351a0394d7d652a4d7456ac9
asoikaw15 <krteammain@gmail.com>
2022-11-02 09:47:26 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+    res.locals.title='Landing Page';
+    res.render('landing')
+  })
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.json())
+
+
+app.listen(process.env.port, () => {
+    console.log(`Example app listening on port ${port}`)  })
